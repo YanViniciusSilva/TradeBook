4fe34417a7f53a073ffc2378d3023ee83ace5162
YanViniciusSilva <yanviniciusjesussilva@gmail.com>
2021-09-20 00:25:36 -0300

    update form style and install bootstrap dependencies

--- src/app/Pages/home/home/createSubscriberForm/createSubscriberForm.component.ts
+++ src/app/Pages/home/home/createSubscriberForm/createSubscriberForm.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit, Injectable, Output, RendererStyleFlags2 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-//import { FormGroup } from '@angular/forms';
-
-//import { apiService } from '../../../../service/api.service';
-import { Subscriber } from 'rxjs';
 import { SubscribersService } from '../../../../service/subscribers.service';
 
 
@@ -19,7 +15,7 @@ import { SubscribersService } from '../../../../service/subscribers.service';
 })
 export class CreateSubscriberFormComponent implements OnInit {
 
-  SubscriberForm!: FormGroup;
+  public SubscriberForm!: FormGroup;
   Submitted = false;
 
   constructor(
@@ -41,10 +37,9 @@ export class CreateSubscriberFormComponent implements OnInit {
       FirstName: [null, [ Validators.required, Validators.minLength(3), Validators.maxLength(255) ]],
       LastName: [null, [ Validators.required, Validators.minLength(3), Validators.maxLength(255) ]],
       Birthday: [null, [ Validators.required]],
-      SubscriberNumber: [null, [ Validators.required, Validators.minLength(11), Validators.maxLength(11), ]],
+      SubscriberNumber: [null, [ Validators.required, Validators.minLength(10), Validators.maxLength(11), ]],
       SubscriberEmail: [null, [ Validators.required, Validators.minLength(3), Validators.maxLength(255), Validators.email ]],
       SubscriberPassword: [null, [ Validators.required, Validators.minLength(3), Validators.maxLength(255) ]],
-      //ConfSubscriberPassword: [null, [ Validators.required, Validators.minLength(3), Validators.maxLength(255) ]],
       SubscriberCep: [null, [ Validators.required, Validators.minLength(8), Validators.maxLength(8) ]],
       SubscriberStreet: [null, [ Validators.required, Validators.minLength(3), Validators.maxLength(255) ]],
       SubscriberStreetNumber: [null, [ Validators.required, Validators.minLength(1), Validators.maxLength(11) ]],
@@ -63,10 +58,18 @@ export class CreateSubscriberFormComponent implements OnInit {
       this.Submitted = true;
       console.log(this.SubscriberForm.value);
       if (this.SubscriberForm.valid){
-        console.log( 'submit');
+        console.log('submit');
+        this.SubscribersService.create(this.SubscriberForm.value).subscribe(
+          error => console.log(error),
+          success => console.log('success'),
+        );
       }
 
     }
+
+
+
+
 
 
 
--- src/app/service/subscribers.service.ts
+++ src/app/service/subscribers.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +10,21 @@ export class SubscribersService {
 
   ServerUrl = 'http://localhost:3001';
 
+
   constructor(
     private http: HttpClient
   ) {  }
 
   public getSubscribers() {
     return this.http.get(`${this.ServerUrl}/Subscribers`);
-    console.log(this.getSubscribers);
   }
 
-  // public postSubscribers(){
-  //   return this.http.post(`${this.ServerUrl}/Subscribers`.Subscriber);
+  create(criar: any){
+    return this.http.post(`${this.ServerUrl}/Subscribers`, criar).pipe(
 
-  // }
+    );
+  }
+
 }
+
+
